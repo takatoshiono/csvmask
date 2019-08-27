6684d05d67976b1ae688fd0d7b6cf09064521f8d
Takatoshi Ono <takatoshi.ono@gmail.com>
2019-08-28 00:13:06 +0900

    empty string

--- mask.go
+++ mask.go
@@ -8,6 +8,9 @@ import (
 
 // right masks n characters to the right of s with c.
 func right(n, c, s string) (string, error) {
+	if s == "" {
+		return s, nil
+	}
 	cnt, err := strconv.ParseInt(n, 10, 32)
 	if err != nil {
 		return s, fmt.Errorf("failed to parse int: %v", err)
